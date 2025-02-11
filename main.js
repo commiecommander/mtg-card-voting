88aa9ddf76d9aa3099f7c47a088cedd835b67e6e
Commie Commander <commie.commander.mtg@gmail.com>
2025-02-11 14:06:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ FingerprintJS.load()
   .then((result) => {
     const fingerprint = result.visitorId; // Unique fingerprint
     localStorage.setItem('fingerprint', fingerprint); // Store fingerprint in localStorage
+    loadCards(); // Load cards after fingerprint is generated
   })
   .catch((error) => {
     console.error("Error generating fingerprint:", error);
@@ -18,7 +19,7 @@ FingerprintJS.load()
 async function fetchVotedCards(fingerprint) {
   try {
     const response = await fetch(
-      "https://script.google.com/macros/s/AKfycby9GxLAK01t0eMQaA6MdCXRKmtFf2zX5gn-Ayx3mvavNft5C_5VzQfar4kT1eW58TOo/exec?action=getVotedCards&fingerprint=" + fingerprint,
+      "https://script.google.com/macros/s/YOUR_GOOGLE_SCRIPT_URL/exec?action=getVotedCards&fingerprint=" + fingerprint,
       {
         method: "GET",
         mode: 'no-cors',
@@ -140,7 +141,7 @@ async function vote(cardName, powerLevel, event) {
   try {
     // Send vote to backend with fingerprint
     const response = await fetch(
-      "https://script.google.com/macros/s/AKfycby9GxLAK01t0eMQaA6MdCXRKmtFf2zX5gn-Ayx3mvavNft5C_5VzQfar4kT1eW58TOo/exec",
+      "https://script.google.com/macros/s/YOUR_GOOGLE_SCRIPT_URL/exec",
       {
         method: "POST",
         headers: {
@@ -172,4 +173,15 @@ async function vote(cardName, powerLevel, event) {
 }
 
 // Load the first set of cards when the page loads
-window.onload = loadCards;
+window.onload = () => {
+  FingerprintJS.load()
+    .then((fp) => fp.get())
+    .then((result) => {
+      const fingerprint = result.visitorId;
+      localStorage.setItem('fingerprint', fingerprint);
+      loadCards();
+    })
+    .catch((error) => {
+      console.error("Error generating fingerprint:", error);
+    });
+};
